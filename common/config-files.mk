dc327de4cdfd1acb86373d7f839af9e3bb5c6e28
Andreas Schneider <asn@cryptomilk.org>
2017-11-20 19:20:06 +0100

    config-files: Install stagefright media files

--- common/config-files.mk
+++ common/config-files.mk
@@ -22,3 +22,9 @@ PRODUCT_COPY_FILES += \
 ### ISRC
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/config/irsc/sec_config:system/etc/sec_config
+
+### STAGEFRIGHT
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
