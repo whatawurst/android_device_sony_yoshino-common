c9b89ed2e5ff4d2e1ebd091e941731c573462871
Andreas Schneider <asn@cryptomilk.org>
2017-11-20 07:26:21 +0100

    common: Add permissions

--- common.mk
+++ common.mk
@@ -5,4 +5,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/ramdisk/init.common.rc:root/init.common.rc \
     $(COMMON_PATH)/ramdisk/init.common.srv.rc:root/init.common.srv.rc
 
+include $(COMMON_PATH)/common/*.mk
+
 $(call inherit-product, device/sony/common-treble/treble.mk)
