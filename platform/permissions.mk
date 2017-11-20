dab5a3c3aef2da7ca621bafeffc43ebbd248ff50
Andreas Schneider <asn@cryptomilk.org>
2017-11-20 07:39:55 +0100

    permissions: Add hardware permissions

---
+++ platform/permissions.mk
@@ -0,0 +1,6 @@
+### PERMISSIONS
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
