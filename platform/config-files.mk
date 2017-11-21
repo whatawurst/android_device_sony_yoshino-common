fe0b890c2e6ce1602f6b5413ec33bf528d8fd43d
Andreas Schneider <asn@cryptomilk.org>
2017-11-21 10:03:46 +0100

    config-files: Add media config files

--- platform/config-files.mk
+++ platform/config-files.mk
@@ -14,3 +14,11 @@ PRODUCT_COPY_FILES += \
 ### AUDIO
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+
+### MEDIA
+ifeq ($(WITH_VENDOR_IMAGE),true)
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/media/media_codecs.xml:vendor/etc/media_codecs.xml \
+    $(PLATFORM_PATH)/config/media/media_codecs_performance.xml:vendor/etc/media_codecs_performance.xml \
+    $(PLATFORM_PATH)/config/media/media_profiles_V1_0.xml:vendor/etc/media_profiles_V1_0.xml
+endif
