6489c25e17370e3f98255475b4d5be43a9cd4f30
Andreas Schneider <asn@cryptomilk.org>
2017-11-24 14:39:59 +0100

    system_prop: Add sensor debug settings

--- common.mk
+++ common.mk
@@ -6,5 +6,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/ramdisk/init.common.srv.rc:root/init.common.srv.rc
 
 include $(COMMON_PATH)/common/*.mk
+include $(COMMON_PATH)/system_prop.mk
 
 $(call inherit-product, device/sony/common-treble/treble.mk)
---
+++ system_prop.mk
@@ -0,0 +1,7 @@
+### SENSORS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.debug.sensors.hal=0 \
+    debug.vendor.sns.daemon=0 \
+    debug.vendor.sns.hal=0 \
+    debug.vendor.sns.libsensor1=0
+
