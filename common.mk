d0d53155a0572af9f3192188a1364fbd51f78608
Andreas Schneider <asn@cryptomilk.org>
2017-12-09 13:09:03 +0100

    init: Add qcom init file

--- common.mk
+++ common.mk
@@ -3,6 +3,7 @@ COMMON_PATH := device/sony/common-treble
 ### INIT
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/ramdisk/init.common.rc:root/init.common.rc \
+    $(COMMON_PATH)/ramdisk/init.common.qcom.rc:root/init.common.qcom.rc \
     $(COMMON_PATH)/ramdisk/init.common.srv.rc:root/init.common.srv.rc
 
 # For android_filesystem_config.h permissions
