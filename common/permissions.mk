f0a5075f2ea8b2b8d360b19a064b6891ac828ab0
Andreas Schneider <asn@cryptomilk.org>
2017-12-12 18:15:25 +0100

    permission: Add audio pro permission

--- common/permissions.mk
+++ common/permissions.mk
@@ -1,6 +1,7 @@
 ### PERMISSIONS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:system/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
