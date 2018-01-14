248df9af95180bd9a634ce7dfebe749880af752c
Andreas Schneider <asn@cryptomilk.org>
2018-01-14 17:15:56 +0100

    hardware: Update hardware files
    We use vendor blobs for now

--- common/hardware.mk
+++ common/hardware.mk
@@ -17,20 +17,8 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
-    libaudio-resampler
-
-### THERMMANAGER
-PRODUCT_PACKAGES += \
-    thermanager
-
-### TIMEKEEP
-PRODUCT_PACKAGES += \
-    timekeep \
-    TimeKeep \
-
-### BLUETOOTH
-PRODUCT_PACKAGES += \
-    macaddrsetup
+    libaudio-resampler \
+    tinymix
 
 ### NFC
 PRODUCT_PACKAGES += \
