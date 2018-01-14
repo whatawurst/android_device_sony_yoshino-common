983cc47c5162c63391663692cdec2c57d8060517
Andreas Schneider <asn@cryptomilk.org>
2018-01-14 17:15:01 +0100

    config-files: Update config and remove obsolete files

--- common/config-files.mk
+++ common/config-files.mk
@@ -12,41 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-### SENSORS
+#################### SYSTEM ######################
+
+# SENSORS
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/config/sensors/sensors_settings:system/etc/sensors/sensors_settings
 
-### AUDIO
+# NFC
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/config/audio/audio_effects.xml:system/etc/audio_effects.xml
+    $(COMMON_PATH)/config/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_configuration_generic.xml:system/etc/audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/primary_audio_policy_configuration.xml:system/etc/primary_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
-
-### QMI
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/config/qmi/dsi_config.xml:system/etc/data/dsi_config.xml \
-    $(COMMON_PATH)/config/qmi/netmgr_config.xml:system/etc/data/netmgr_config.xml \
-    $(COMMON_PATH)/config/qmi/qmi_config.xml:system/etc/data/qmi_config.xml
-
-### ISRC
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/config/irsc/sec_config:system/etc/sec_config
-
-### STAGEFRIGHT
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
-
-### GPS
+# GPS
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/config/gps/gps.conf:system/etc/gps.conf
 
-### NFC
+#################### VENDOR ######################
+
+# AUDIO
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/config/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
+    $(COMMON_PATH)/config/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+
+# ISRC
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/config/irsc/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
+# STAGEFRIGHT
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
