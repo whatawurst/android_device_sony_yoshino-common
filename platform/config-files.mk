17cb962e6ea4466b23fb52e690a231a38d9e7d50
Andreas Schneider <asn@cryptomilk.org>
2018-01-18 14:04:59 +0100

    config-files: Remove rild.rc

--- platform/config-files.mk
+++ platform/config-files.mk
@@ -59,7 +59,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(PLATFORM_PATH)/config/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
-
-### RIL
-PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/ril/rild.rc:system/etc/init/rild.rc
