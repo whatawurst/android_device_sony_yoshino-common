ad25bcc388507d9be535492a0594008705d6f091
Andreas Schneider <asn@cryptomilk.org>
2018-01-25 14:18:55 +0100

    ueventd: Update and install to correct location

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -43,8 +43,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ueventd.$(TARGET_DEVICE)
 LOCAL_SRC_FILES := ueventd.yoshino.rc
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := ueventd.$(TARGET_DEVICE)
+LOCAL_MODULE_STEM := ueventd
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+# This needs to be /vendor/ueventd.rc
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
