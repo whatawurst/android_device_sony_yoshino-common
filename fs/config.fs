dca2a35ce34add487e5ca17fcbc49ffb6cef83c5
Andreas Schneider <asn@cryptomilk.org>
2018-01-25 14:30:53 +0100

    fs: Add missing services

--- fs/config.fs
+++ fs/config.fs
@@ -22,11 +22,35 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
+[vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
 [vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
+caps: NET_BIND_SERVICE NET_ADMIN
+
+[vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
 caps: NET_BIND_SERVICE
+
+[vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: WAKE_ALARM
 
 [vendor/bin/pm-service]
 mode: 0755
@@ -38,6 +62,12 @@ caps: NET_BIND_SERVICE
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
 caps: NET_BIND_SERVICE
 
 [vendor/bin/wcnss_filter]
