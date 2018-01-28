51cd63be675519f0454f037e46d609cc20b90a9a
Andreas Schneider <asn@cryptomilk.org>
2018-01-28 13:57:33 +0100

    hardware: Add libdisplayconfig and liboverlay

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -3,7 +3,9 @@ PRODUCT_PACKAGES += \
     copybit.msm8998 \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
-    memtrack.msm8998
+    memtrack.msm8998 \
+    libdisplayconfig \
+    liboverlay
 
 ### AUDIO
 PRODUCT_PACKAGES += \
