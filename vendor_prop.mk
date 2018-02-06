d1b112a4c2388ae72b6f816c76fcfbbeb91685da
Andreas Schneider <asn@cryptomilk.org>
2018-02-06 11:57:55 +0100

    vendor_prop: Add settings or radio

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -48,7 +48,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ### RILD
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.default_network=10 \
+    telephony.lteOnCdmaDevice=1 \
+    ro.use_data_netmgrd=true
 
 # Default to LTE/GSM/WCDMA.
 PRODUCT_PROPERTY_OVERRIDES += \
