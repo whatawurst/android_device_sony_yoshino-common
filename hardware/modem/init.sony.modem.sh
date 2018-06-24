18dafd2420cb8ed28593ee4eb425ffaf7b105cb5
Andreas Schneider <asn@cryptomilk.org>
2018-06-24 16:56:35 +0200

    modem: Fix /oem for CustomizationSelector
    The CustomizationSelector is looking for a directory. So it fails if it
    find a symlink. This uses a bind mount to address the issue.

--- hardware/modem/init.sony.modem.sh
+++ hardware/modem/init.sony.modem.sh
@@ -1,14 +1,27 @@
 #!/vendor/bin/sh
 
-OEM_DIR="/tmp/oem"
+TMP_OEM_DIR="/tmp/oem"
+OEM_DIR="/oem"
 
-if [ ! -d "${OEM_DIR}" ]; then
-    mkdir "${OEM_DIR}"
-fi
+### CLEANUP
+umount ${OEM_DIR} 2>/dev/null
+umount ${TMP_OEM_DIR}/modem-config 2>/dev/null
 
-ln -sf /oem-modem/modem-config ${OEM_DIR}/modem-config
+rm -rf "${TMP_OEM_DIR}"
 
-mount --bind ${OEM_DIR} /oem
-mount -o remount,bind,ro ${OEM_DIR} /oem
+### CREATE TEMPORARY OEM
+mkdir "${TMP_OEM_DIR}"
+chmod 0755 "${TMP_OEM_DIR}"
 
+### MOUNT OEM
+mount --bind "${TMP_OEM_DIR}" "${OEM_DIR}"
+
+### MOUNT MODEM-CONFIG
+mkdir "${OEM_DIR}/modem-config"
+chmod 0755 "${OEM_DIR}/modem-config"
+
+mount --bind /oem-modem/modem-config ${OEM_DIR}/modem-config
+mount -o remount,bind,ro /oem-modem/modem-config ${OEM_DIR}/modem-config
+
+### DONE
 exit 0
