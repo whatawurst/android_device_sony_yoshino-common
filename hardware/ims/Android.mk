f6f925d5cf49d488dec544f51b79dc9540ec4fac
Andreas Schneider <asn@cryptomilk.org>
2018-07-30 09:27:10 +0200

    hardware: Add ims app symlinks

---
+++ hardware/ims/Android.mk
@@ -0,0 +1,25 @@
+ifeq ($(BOARD_IMS_CAMERA),true)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+IMS_CAMERA_SYMLINK := $(TARGET_OUT)/app/ims/lib/arm64/libimscamera_jni.so
+$(IMS_CAMERA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Create ims camera jni link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/libimscamera_jni.so $@
+
+include $(CLEAR_VARS)
+IMS_MEDIA_SYMLINK := $(TARGET_OUT)/app/ims/lib/arm64/libimsmedia_jni.so
+$(IMS_MEDIA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Create ims media jni link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/libimsmedia_jni.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(IMS_CAMERA_SYMLINK) \
+	$(IMS_MEDIA_SYMLINK)
+
+endif
