4879fcba071724575fb19e2f0317d20667766353
Andreas Schneider <asn@cryptomilk.org>
2018-08-22 15:55:53 +0200

    config: Add qti privapp permissions

--- common/permissions.mk
+++ common/permissions.mk
@@ -14,6 +14,9 @@
 
 ### PERMISSIONS
 PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
