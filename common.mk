b590d19e03028bdaf9ff905d94215a098d833216
Andreas Schneider <asn@cryptomilk.org>
2019-02-12 17:30:22 +0100

    common: Move init scripts to /vendor

--- common.mk
+++ common.mk
@@ -16,10 +16,10 @@ COMMON_PATH := device/sony/common-treble
 
 ### INIT
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/ramdisk/init.common.rc:root/init.common.rc \
-    $(COMMON_PATH)/ramdisk/init.common.qcom.rc:root/init.common.qcom.rc \
-    $(COMMON_PATH)/ramdisk/init.common.srv.rc:root/init.common.srv.rc \
-    $(COMMON_PATH)/ramdisk/init.common.ims.rc:root/init.common.ims.rc
+    $(COMMON_PATH)/config/init/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.rc \
+    $(COMMON_PATH)/config/init/init.common.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.qcom.rc \
+    $(COMMON_PATH)/config/init/init.common.srv.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.srv.rc \
+    $(COMMON_PATH)/config/init/init.common.ims.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.ims.rc
 
 # For android_filesystem_config.h permissions
 PRODUCT_PACKAGES += \
