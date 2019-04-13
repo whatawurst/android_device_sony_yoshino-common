1bfb39b8f9bc16a7a131cb51c784800dc88c8db4
derfelot <frederic.koch@gmail.com>
2019-04-13 12:23:29 +0200

    board: Add firmware_class.path to kernel commandline

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -54,6 +54,7 @@ BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += zram.backend=z3fold
 BOARD_KERNEL_CMDLINE += buildvariant=userdebug
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 
 # See README_Xperia in Kernel Source
 BOARD_KERNEL_BASE        := 0x00000000
