b105fe7fdfb05141321a19f6ddba662652a246d2
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    platform: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- platform.mk
+++ platform.mk
@@ -16,6 +16,9 @@
 
 $(call inherit-product, device/sony/common-treble/common.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 PLATFORM_PATH := device/sony/yoshino
 
 ### PLATFORM INIT
