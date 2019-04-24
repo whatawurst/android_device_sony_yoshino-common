75086ece7aa2b052eda76db1ab48db92dbabd660
Andreas Schneider <asn@cryptomilk.org>
2019-04-24 12:29:12 +0200

    twrp: Fix manifest, fstab and add missing blobs

--- platform.mk
+++ platform.mk
@@ -38,7 +38,8 @@ PRODUCT_COPY_FILES += \
 
 # Add manifest for hwservicemanager
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/recovery/vendor/manifest.xml:recovery/root/vendor/manifest.xml
+    $(PLATFORM_PATH)/recovery/root/system/etc/vintf/manifest.xml:recovery/root/system/etc/vintf/manifest.xml \
+    $(PLATFORM_PATH)/recovery/root/vendor/etc/vintf/manifest.xml:recovery/root/vendor/etc/vintf/manifest.xml
 
 else # WITH_TWRP
 ### VERITY
