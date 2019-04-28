99bc0a2d87c749f754106b15b7e5b3b825c317d5
Andreas Schneider <asn@cryptomilk.org>
2019-04-28 13:46:48 +0200

    platform: Update livedisplay to 2.0
    This doesn't turn on the AdaptiveBacklight interface which is by default
    disabled anyway.

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -22,7 +22,8 @@ PRODUCT_PACKAGES += \
 
 # DISPLAY
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # MODEM
 PRODUCT_PACKAGES += \
