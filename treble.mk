b831f90f580c383c9a65095709662d8f48774ce4
Andreas Schneider <asn@cryptomilk.org>
2019-04-28 18:36:14 +0200

    treble: Build clearkey and widevine drm services

--- treble.mk
+++ treble.mk
@@ -133,7 +133,9 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.1-service.widevine
 
 # USB
 PRODUCT_PACKAGES += \
