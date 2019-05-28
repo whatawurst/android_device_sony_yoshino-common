33aa893e5cb0a962b972f6f0e1261ec863d80f4d
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    yoshino-common: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -209,7 +209,7 @@ TARGET_HW_DISK_ENCRYPTION := true
 
 ### SEPOLICY
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/sony/yoshino-common/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += device/sony/yoshino-common/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/sony/yoshino-common/sepolicy/private
 
 ### RECOVERY
