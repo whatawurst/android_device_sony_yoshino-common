c843cd2141b48f9c2abc800c90ec97000150b173
derfelot <5124143+derfelot@users.noreply.github.com>
2019-08-31 02:43:27 +0200

    hardware: Generate adreno symlinks

---
+++ hardware/adreno/Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LIBEGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so
+$(LIBEGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/libEGL_adreno.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+LIBEGL64_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/libEGL_adreno.so
+$(LIBEGL64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/libEGL_adreno.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+LIBQ3DTOOLS_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libq3dtools_adreno.so
+$(LIBQ3DTOOLS_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/libq3dtools_adreno.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+LIBQ3DTOOLS64_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/libq3dtools_adreno.so
+$(LIBQ3DTOOLS64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/libq3dtools_adreno.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(LIBEGL_SYMLINK) \
+	$(LIBEGL64_SYMLINK) \
+	$(LIBQ3DTOOLS_SYMLINK) \
+	$(LIBQ3DTOOLS64_SYMLINK)
