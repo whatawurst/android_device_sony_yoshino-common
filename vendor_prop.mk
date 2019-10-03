aa6b1005b566441f227898df6838ea57740e60b6
derfelot <5124143+derfelot@users.noreply.github.com>
2019-10-03 17:24:50 +0200

    vendor_prop: Alphabetize props and categories
    No props changed, added or removed

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -12,117 +12,119 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-### SENSORS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.gyro.disable=0 \
-    persist.vendor.debug.sensors.hal=0 \
-    debug.vendor.sns.daemon=0 \
-    debug.vendor.sns.libsensor1=0
-
 ### AUDIO
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.aac_51_output_enabled=true \
     audio.deep_buffer.media=true \
-    ro.config.vc_call_vol_steps=8 \
-    ro.config.media_vol_steps=30
+    media.aac_51_output_enabled=true \
+    ro.config.media_vol_steps=30 \
+    ro.config.vc_call_vol_steps=8
 
-### DRM SERVICE
+### DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
-### WFD
-# Property to enable user to access Google WFD settings.
+### GPS
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1
+    persist.vendor.overlay.izat.optin=rro
 
-# Property for WfdService.apk to fix WFD for some apps when 
-# hdcp is not provisioned (e.g. unlocked bootloader)
+### IMS
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.appmonitoring=1
+    vendor.service.qti.ims.enabled=1
 
-# Property to choose between virtual/external wfd display
+### NFC
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.wfd.virtual=0
+    ro.hardware.nfc_nci=nqx.default \
+    ro.nfc.on.default=false \
+    ro.nfc.port=I2C \
+    ro.nfc.se.sim.enable=true \
+    ro.nfc.se.smx.enable=false
 
-### RILD
+### RADIO
+# CNE
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM \
-    telephony.lteOnCdmaDevice=1
+    persist.vendor.cne.feature=1
 
-# Enable netmgr
+# DPM module
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.use_data_netmgrd=true \
-    persist.vendor.radio.aosp_usr_pref_sel=true \
+    persist.vendor.dpm.feature=1
+
+# Netmgr
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
-    persist.vendor.data.mode=concurrent
+    persist.vendor.data.mode=concurrent \
+    persist.vendor.radio.aosp_usr_pref_sel=true \
+    ro.vendor.use_data_netmgrd=true
 
+# Rild
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rild.nitz_plmn="" \
+    ril.subscription.types=NV,RUIM \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so
+
+# Rild - NITZ
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_long_ons_0="" \
     persist.rild.nitz_long_ons_1="" \
     persist.rild.nitz_long_ons_2="" \
     persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_plmn="" \
     persist.rild.nitz_short_ons_0="" \
     persist.rild.nitz_short_ons_1="" \
     persist.rild.nitz_short_ons_2="" \
     persist.rild.nitz_short_ons_3=""
 
-# Enable CNE
+# Rmnet data
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.cne.feature=1
-
-# Enable dpm module
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.dpm.feature=1
-
-# Enable rmnet data
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rmnet.data.enable=true \
-    persist.data.wda.enable=true \
-    persist.data.df.dl_mode=5 \
-    persist.data.df.ul_mode=5 \
     persist.data.df.agg.dl_pkt=10 \
     persist.data.df.agg.dl_size=4096 \
-    persist.data.df.mux_count=8 \
+    persist.data.df.dl_mode=5 \
+    persist.data.df.dev_name=rmnet_usb0 \
     persist.data.df.iwlan_mux=9 \
-    persist.data.df.dev_name=rmnet_usb0
+    persist.data.df.mux_count=8 \
+    persist.data.df.ul_mode=5 \
+    persist.data.wda.enable=true \
+    persist.rmnet.data.enable=true
 
-# Buffer sizes
+# TCP Buffer sizes
 PRODUCT_PROPERTY_OVERRIDES += \
+    net.tcp.2g_init_rwnd=10 \
     net.tcp.buffersize.default=4096,87380,524288,4096,16384,110208 \
-    net.tcp.buffersize.lte=2097152,4194304,8388608,262144,524288,1048576 \
-    net.tcp.buffersize.umts=4094,87380,110208,4096,16384,110208 \
-    net.tcp.buffersize.hspa=4094,87380,1220608,4096,16384,1220608 \
-    net.tcp.buffersize.hsupa=4094,87380,1220608,4096,16384,1220608 \
-    net.tcp.buffersize.hsdpa=4094,87380,1220608,4096,16384,1220608 \
-    net.tcp.buffersize.hspap=4094,87380,1220608,4096,16384,1220608 \
     net.tcp.buffersize.edge=4093,26280,35040,4096,16384,35040 \
-    net.tcp.buffersize.gprs=4092,8760,11680,4096,8760,11680 \
     net.tcp.buffersize.evdo=4094,87380,524288,4096,16384,262144 \
-    net.tcp.2g_init_rwnd=10
-
-### NFC
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.nfc_nci=nqx.default \
-    ro.nfc.port=I2C \
-    ro.nfc.se.sim.enable=true \
-    ro.nfc.se.smx.enable=false \
-    ro.nfc.on.default=false
-
-### WIFI
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0 \
-    net.tcp.buffersize.wifi=524288,2097152,4194304,262144,524288,1048576
-
-# IMS
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.service.qti.ims.enabled=1
-
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.overlay.izat.optin=rro
+    net.tcp.buffersize.gprs=4092,8760,11680,4096,8760,11680 \
+    net.tcp.buffersize.hsdpa=4094,87380,1220608,4096,16384,1220608 \
+    net.tcp.buffersize.hspa=4094,87380,1220608,4096,16384,1220608 \
+    net.tcp.buffersize.hspap=4094,87380,1220608,4096,16384,1220608 \
+    net.tcp.buffersize.hsupa=4094,87380,1220608,4096,16384,1220608 \
+    net.tcp.buffersize.lte=2097152,4194304,8388608,262144,524288,1048576 \
+    net.tcp.buffersize.umts=4094,87380,110208,4096,16384,110208
 
 # Telephony
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.call_ring.multiple=false
+    ro.telephony.call_ring.multiple=false \
+    telephony.lteOnCdmaDevice=1
+
+### SENSORS
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.vendor.sns.daemon=0 \
+    debug.vendor.sns.libsensor1=0 \
+    persist.camera.gyro.disable=0 \
+    persist.vendor.debug.sensors.hal=0
+
+### WFD
+# Property for WfdService.apk to fix WFD for some apps when 
+# hdcp is not provisioned (e.g. unlocked bootloader)
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.appmonitoring=1
+
+# Property to enable user to access Google WFD settings.
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1
+
+# Property to choose between virtual/external wfd display
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.wfd.virtual=0
+
+### WIFI
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tcp.buffersize.wifi=524288,2097152,4194304,262144,524288,1048576 \
+    wifi.interface=wlan0
