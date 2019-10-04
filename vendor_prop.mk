66f3b863686862fc9637fa3db397860f32197dcf
derfelot <5124143+derfelot@users.noreply.github.com>
2019-10-04 21:50:13 +0200

    yoshino: Rearrange some props between system and vendor
    According to where they are located in stock

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -27,18 +27,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio_hal.period_size=192
 
-# Deep buffer enable
-PRODUCT_PROPERTY_OVERRIDES += \
-    audio.deep_buffer.media=true
-
 # DS2, Hardbypass feature for Dolby disable
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false
-
-# Fast audio output client buffer size reduction
-PRODUCT_PROPERTY_OVERRIDES += \
-    af.fast_track_multiplier=1
 
 # Flac sw decoder 24 bit decode capability
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -88,10 +80,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.offload.pstimeout.secs=3
 
-# Offload video playback disable
-PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.video=false
-
 # Parser input buffer size (256kb) in byte stream mode
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.parser.ip.buffer.size=262144
@@ -118,11 +106,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 ### BLUETOOTH
 PRODUCT_PROPERTY_OVERRIDES += \
-    bt.max.hfpclient.connections=1 \
     ro.vendor.bt.bdaddr_path="/data/vendor/etc/bluetooth_bdaddr" \
     ro.vendor.btstack.hfp.ver=1.7 \
     persist.vendor.bt.enable.splita2dp=false \
-    persist.vendor.btstack.a2dp_offload_cap=false \
     persist.vendor.service.bdroid.ssrlvl=3 \
     vendor.qcom.bluetooth.soc=cherokee
 
@@ -131,26 +117,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1
 
 ### DISPLAY
-# Assertive display
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qcom.ad=1 \
-    ro.vendor.display.ad=1 \
-    ro.vendor.display.ad.sdr_calib_data=/vendor/etc/ad_calib.cfg \
-    ro.vendor.display.sensortype=2
-
 # CABL
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qualcomm.cabl=0 \
     ro.vendor.display.cabl=2
 
-# Display default color mode enable
+# Display Properties as per treble compliance
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.display.enable_default_color_mode=1
+    vendor.gralloc.disable_ubwc=0 \
+    vendor.gralloc.enable_fb_ubwc=1
 
 # HDR
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.display.disable_hdr_lut_gen=1 \
-    vendor.vidc.enc.disable.pq=true
+    vendor.display.disable_hdr_lut_gen=1
 
 # OpenGLES version
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -163,15 +141,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Virtual keys
 PRODUCT_PROPERTY_OVERRIDES += \
     qemu.hw.mainkeys=0
-
-# Xfrm support
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.service.xrfm.mode=1 \
-    ro.service.xrfm.supported=true
-
-### MEDIA PROFILES
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.settings.xml=/vendor/etc/media_profiles_vendor.xml
 
 ### MISC
 # Factory reset protection path
@@ -186,23 +155,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
 
-# SD Encryption supported
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sdcrypt.supported=true
-
 ### PERFORMANCE
-# Core control max/min CPU's
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.core_ctl_max_cpu=4 \
-    ro.vendor.qti.core_ctl_min_cpu=2
-
 # Power balancing
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
-
-# Sched colocation
-PRODUCT_PROPERTY_OVERRIDES += \
-    sched.colocate.enable=1
 
 ### RADIO
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -233,49 +189,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.add_power_save=1
 
-### SENSORS
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.sdk.sensors.gestures=false \
-    ro.vendor.sensors.amd=false \
-    ro.vendor.sensors.bte=false \
-    ro.vendor.sensors.cmc=false \
-    ro.vendor.sensors.dev_ori=true \
-    ro.vendor.sensors.dpc=true \
-    ro.vendor.sensors.facing=false \
-    ro.vendor.sensors.fast_amd=false \
-    ro.vendor.sensors.fns=false \
-    ro.vendor.sensors.game_rv=true \
-    ro.vendor.sensors.georv=true \
-    ro.vendor.sensors.gravity=true \
-    ro.vendor.sensors.gtap=false \
-    ro.vendor.sensors.iod=false \
-    ro.vendor.sensors.laccel=true \
-    ro.vendor.sensors.mot_detect=true \
-    ro.vendor.sensors.multishake=true \
-    ro.vendor.sensors.orientation=true \
-    ro.vendor.sensors.pam=false \
-    ro.vendor.sensors.pedometer=false \
-    ro.vendor.sensors.pmd=true \
-    ro.vendor.sensors.proximity=true \
-    ro.vendor.sensors.pug=false \
-    ro.vendor.sensors.qheart=false \
-    ro.vendor.sensors.qmd=false \
-    ro.vendor.sensors.rawdata_mode=false \
-    ro.vendor.sensors.rmd=false \
-    ro.vendor.sensors.rotvec=true \
-    ro.vendor.sensors.scrn_ortn=false \
-    ro.vendor.sensors.smd=true \
-    ro.vendor.sensors.sta_detect=true \
-    ro.vendor.sensors.step_counter=true \
-    ro.vendor.sensors.step_detector=true \
-    ro.vendor.sensors.tap=false \
-    ro.vendor.sensors.tilt=false \
-    ro.vendor.sensors.tilt_detector=true \
-    ro.vendor.sensors.vmd=false \
-    ro.vendor.sensors.wrist_tilt=false \
-    ro.vendor.sensors.wu=true \
-    ro.vendor.qf_use_report_period=false
-
 ### THERMAL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.thermal=somc
@@ -283,5 +196,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ### USB
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.usb.config.extra=none \
-    sys.usb.controller=a800000.dwc3 \
     vendor.usb.rndis.func.name=gsi
