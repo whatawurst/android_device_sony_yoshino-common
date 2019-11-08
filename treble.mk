af3b62037314a22e621e885919a3d1ebd39d8daa
derfelot <5124143+derfelot@users.noreply.github.com>
2019-11-09 00:26:29 +0100

    treble: Uprev soundtrigger to 2.2

--- treble.mk
+++ treble.mk
@@ -28,7 +28,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
     android.hardware.soundtrigger@2.0-service
 
 # CAMERA
