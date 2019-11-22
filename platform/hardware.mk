c5ee5dd05b6dbf17a3c5880c55dbd2d64b80c7f9
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    yoshino-common: Switch to Audio HAL V6
    Switch to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -65,9 +65,9 @@ PRODUCT_PACKAGES += \
 ### SERVICES
 # AUDIO
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.soundtrigger@2.0-service
