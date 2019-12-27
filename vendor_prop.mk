339a9c6a497b842d7a31477c1ac0928f8ae64d14
derfelot <5124143+derfelot@users.noreply.github.com>
2019-12-27 21:31:35 +0100

    vendor_prop: Add vsync offsets from op5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -155,7 +155,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
 # Touch
 PRODUCT_PROPERTY_OVERRIDES += \
