c32366ec6198e9f357bf809b9d209e2a411b8384
derfelot <5124143+derfelot@users.noreply.github.com>
2019-12-31 05:07:41 +0100

    hardware: Generate default modem symlink based on device

--- hardware/modem/Android.mk
+++ hardware/modem/Android.mk
@@ -7,9 +7,26 @@ CUSTOMIZATION_MODEM_SYMLINKS := $(TARGET_OUT)/etc/customization/modem
 $(CUSTOMIZATION_MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Create customization modem links: $@"
 	@mkdir -p $@
-	$(hide) ln -sf amss_fsg_lilac_tar.mbn $@/default
 	$(hide) ln -sf amss_fs_empty.mbn $@/reset_modemst1
 	$(hide) ln -sf amss_fs_empty.mbn $@/reset_modemst2
+ifeq ($(TARGET_DEVICE),lilac)
+	$(hide) ln -sf amss_fsg_lilac_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),poplar)
+	$(hide) ln -sf amss_fsg_poplar_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),poplar_canada)
+	$(hide) ln -sf amss_fsg_poplar_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),poplar_dsds)
+	$(hide) ln -sf amss_fsg_poplar_dsds_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),maple)
+	$(hide) ln -sf amss_fsg_maple_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),maple_dsds)
+	$(hide) ln -sf amss_fsg_maple_dsds_tar.mbn $@/default
+endif
 
 ALL_DEFAULT_INSTALLED_MODULES += \
 	$(CUSTOMIZATION_MODEM_SYMLINKS)
