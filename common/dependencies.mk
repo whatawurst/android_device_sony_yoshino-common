bd6d4b9557ac3876a1b82ea7ae51ce6ab97894c6
derfelot <5124143+derfelot@users.noreply.github.com>
2020-02-16 04:59:19 +0100

    treble: Build missing telephony/ims packages

--- common/dependencies.mk
+++ common/dependencies.mk
@@ -46,7 +46,9 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common_system
+    ims-ext-common \
+    ims-ext-common_system \
+    ims_ext_common.xml
 
 PRODUCT_BOOT_JARS += \
     ims-ext-common_system
@@ -91,7 +93,11 @@ PRODUCT_PACKAGES += \
 
 ### TELEPHONY
 PRODUCT_PACKAGES += \
-    telephony-ext
+    telephony-ext \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
