189c4a82a05b676a6d0a15c90aafaca8b07e48d9
derfelot <derfelot.git@gmail.com>
2020-08-12 21:25:21 +0200

    Revert "hardware: Use new ASOP bluetooth audio HAL v2"
    This reverts commit bbef02f4a4aaf11e9a9b8dd418d32d760f4b198c.

--- common/hardware.mk
+++ common/hardware.mk
@@ -14,15 +14,9 @@
 
 ### AUDIO
 PRODUCT_PACKAGES += \
+    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default
-
-PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    audio.bluetooth.default \
-    android.hardware.bluetooth.a2dp@1.0 \
-    android.hardware.bluetooth.audio@2.0 \
-    android.hardware.bluetooth.audio@2.0-impl
 
 ### CAMERA
 PRODUCT_PACKAGES += \
