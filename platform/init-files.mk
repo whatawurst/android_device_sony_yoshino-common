e9521d7bf32408fb43a9c2dbe8795357c76bbec3
derfelot <derfelot.git@gmail.com>
2020-10-12 02:54:55 +0200

    yoshino: Move more init files into init-files.mk
    Basically all those that don't require a renaming to TARGET_DEVICE

--- platform/init-files.mk
+++ platform/init-files.mk
@@ -1,7 +1,21 @@
-# Modified init.qcom.early_boot.sh
+# fstab
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/init/idd.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/idd.fstab \
+    $(PLATFORM_PATH)/config/init/qns.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/qns.fstab
+
+# init rc
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/init/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.rc \
     $(PLATFORM_PATH)/config/init/init.common.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.qcom.rc \
     $(PLATFORM_PATH)/config/init/init.common.srv.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.srv.rc \
     $(PLATFORM_PATH)/config/init/init.common.ims.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.ims.rc \
+    $(PLATFORM_PATH)/config/init/init.yoshino.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.usb.rc \
+    $(PLATFORM_PATH)/config/init/init.yoshino.pwr.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.pwr.rc
+
+# init.qcom.early_boot.sh (modified)
+PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/init/init.qcom.early_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.early_boot.sh
+
+# ueventd
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/init/ueventd.yoshino.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
