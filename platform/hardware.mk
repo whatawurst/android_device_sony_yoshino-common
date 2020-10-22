c2980d60d1a0cd7074aa160c56a507b9ca5c802b
derfelot <derfelot.git@gmail.com>
2020-10-22 22:07:20 +0200

    yoshino-common: Fix secure_element crashes
    Don't build android.hardware.secure_element@1.0-service
    
    This should still allow to use sim's as secure storage. Whether it actually works I don't know, as I am lacking a test case.

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -48,6 +48,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     NfcNci \
+    SecureElement \
     Tag
 
 ### OPENCUSTOMIZATIONSELECTOR
@@ -126,11 +127,6 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service
-
-# NFC - SECURE ELEMENT
-PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.0-service \
-    SecureElement
 
 # POWER
 PRODUCT_PACKAGES += \
