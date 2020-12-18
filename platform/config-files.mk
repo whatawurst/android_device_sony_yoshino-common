08fa38d5d9f4a8bd4a93ee0ca1cdc56119bd900f
derfelot <derfelot.git@gmail.com>
2020-12-18 02:53:53 +0100

    yoshino-common: Import perfboostsconfig.xml from vendor
    So we can modify it to our liking.

--- platform/config-files.mk
+++ platform/config-files.mk
@@ -34,6 +34,10 @@ PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/nfc/libnfc-nxp-octopus.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-octopus.conf \
     $(PLATFORM_PATH)/config/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+### PERF
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
+
 ### POWER
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
