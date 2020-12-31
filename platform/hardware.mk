c412ef72e7c3b00f6f2d67a61c7a0bdc0a9855dc
derfelot <derfelot.git@gmail.com>
2020-12-31 10:04:10 +0100

    yoshino-common: Opt-out of full QTI BT stack
    Also, don't build libbt-vendor and bthost_if anymore.

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -17,8 +17,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
-    libvolumelistener \
-    bthost_if
+    libvolumelistener
 
 ### CHARGER
 PRODUCT_PACKAGES += \
