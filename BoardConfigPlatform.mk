8913473f0b677595ee745c23b78a70166c2dfb16
Georg Veichtlbauer <georg@vware.at>
2021-03-04 09:15:37 +0100

    yoshino-common: Remove BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED
    * It defaults to true
    
    Change-Id: If110ded11c28c7df982a16c5111be215bc451384

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -139,10 +139,6 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += device/sony/yoshino-common/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/sony/yoshino-common/sepolicy/private
 
-### TREBLE
-# Split build properties
-BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
-
 ### VENDOR FILE OVERRIDE
 BUILD_BROKEN_DUP_RULES := true
 
