e03254de539e7e9dfa82c9a90b7b4aabcd0466bb
derfelot <derfelot.git@gmail.com>
2021-03-20 17:51:37 +0100

    yoshino-common: init: Rearrange init scripts
     * Merge init.common.rc into init.yoshino.rc
     * Rename common --> yoshino
     * Move all service definitions to init.yoshino.srv.rc and alphabetize it

--- platform/init-files.mk
+++ platform/init-files.mk
@@ -1,11 +1,10 @@
 # init rc
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/init/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.rc \
-    $(PLATFORM_PATH)/config/init/init.common.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.qcom.rc \
-    $(PLATFORM_PATH)/config/init/init.common.srv.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.srv.rc \
-    $(PLATFORM_PATH)/config/init/init.common.ims.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.common.ims.rc \
-    $(PLATFORM_PATH)/config/init/init.yoshino.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.usb.rc \
-    $(PLATFORM_PATH)/config/init/init.yoshino.pwr.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.pwr.rc
+    $(PLATFORM_PATH)/config/init/init.yoshino.ims.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.ims.rc \
+    $(PLATFORM_PATH)/config/init/init.yoshino.pwr.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.pwr.rc \
+    $(PLATFORM_PATH)/config/init/init.yoshino.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.qcom.rc \
+    $(PLATFORM_PATH)/config/init/init.yoshino.srv.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.srv.rc \
+    $(PLATFORM_PATH)/config/init/init.yoshino.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.usb.rc
 
 # init.qcom.early_boot.sh (modified)
 PRODUCT_COPY_FILES += \
