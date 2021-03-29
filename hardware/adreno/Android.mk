bac6a07536a7768f6fbf5c337f3fb4a049a8e9f4
Andrea <linckandrea01@gmail.com>
2021-03-29 10:18:31 +0200

    yoshino-common: hardware: adreno: add vulkan libs symlink
     * without those maple is unable to load vulkan libs

--- hardware/adreno/Android.mk
+++ hardware/adreno/Android.mk
@@ -26,8 +26,24 @@ $(LIBQ3DTOOLS64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@mkdir -p $(dir $@)
 	$(hide) ln -sf egl/$(notdir $@) $@
 
+VULKAN_LIB_PATH := hw/vulkan.msm8998.so
+
+VULKAN_SYMLINK := $(TARGET_OUT_VENDOR)/lib/vulkan.msm8998.so
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/vulkan.msm8998.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(VULKAN_LIB_PATH) $@
+
+VULKAN64_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/vulkan.msm8998.so
+$(VULKAN64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/vulkan.msm8998.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(VULKAN_LIB_PATH) $@
+
 ALL_DEFAULT_INSTALLED_MODULES += \
 	$(LIBEGL_SYMLINK) \
 	$(LIBEGL64_SYMLINK) \
 	$(LIBQ3DTOOLS_SYMLINK) \
-	$(LIBQ3DTOOLS64_SYMLINK)
+	$(LIBQ3DTOOLS64_SYMLINK) \
+	$(VULKAN_SYMLINK) \
+	$(VULKAN64_SYMLINK)
