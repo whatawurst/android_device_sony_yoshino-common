2f58217ba9f11b307f81a98db7f4b39ba02793ba
derfelot <derfelot.git@gmail.com>
2021-05-02 15:43:12 +0200

    yoshino-common: Build android.hardware.radio.deprecated
    In case we want to switch to stock rild stack, which depends on it.

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -144,7 +144,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.radio@1.1 \
-    android.hardware.radio.config@1.0
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.deprecated@1.0
 
 # RENDERSCRIPT
 PRODUCT_PACKAGES += \
