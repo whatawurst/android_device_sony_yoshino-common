10704d47cb8b27b1ca3188d7ed1c94ddaf2df3d3
Andrea <linckandrea01@gmail.com>
2021-05-30 14:17:47 +0200

    yoshino-common: add TARGET_USE_CUSTOM_POWERHINT to opt from common powerhint
     * right now common powerhint is too OP for lilac

--- platform/config-files.mk
+++ platform/config-files.mk
@@ -53,9 +53,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/irqbalance/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+ifeq ($(TARGET_USE_CUSTOM_POWERHINT),false)
 ### POWER
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+endif
 
 ### QTI_LOW_POWER_WHITELIST
 PRODUCT_COPY_FILES += \
