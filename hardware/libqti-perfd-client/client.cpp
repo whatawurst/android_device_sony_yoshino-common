9642317ea4f4c738e0064eb05f9ca5d042bb5e0a
Alexander Grund <flamefire89@gmail.com>
2021-09-13 18:26:54 +0200

    libqti-perfd-client: Fix function signatures and return values
    - Use correct function signatures
    - Remove namespace (not required due to extern "C"
    - Put extern C into scope
    - Return non-negative dummy values so callers don't see this as failed
    
    Idea from: https://review.lineageos.org/c/LineageOS/android_device_xiaomi_sdm845-common/+/310583
    
    Change-Id: If720187793142b2b8d69f7f20d292830de1fc8aa

--- hardware/libqti-perfd-client/client.cpp
+++ hardware/libqti-perfd-client/client.cpp
@@ -1,10 +1,20 @@
 #include <stdint.h>
 
-namespace android {
-    extern "C" void perf_get_feedback() {}
-    extern "C" void perf_hint() {}
-    extern "C" void perf_lock_acq() {}
-    extern "C" void perf_lock_cmd() {}
-    extern "C" void perf_lock_rel() {}
-    extern "C" void perf_lock_use_profile() {}
+extern "C" {
+    void perf_get_feedback() {}
+    int perf_hint(int /*hint*/, const char* /*pkg*/, int /*duration*/, int /*type*/) {
+        return 233;
+    }
+    int perf_lock_acq(int handle, int /*duration*/, int* /*hints*/, int /*num_args*/) {
+        if (handle > 0)
+            return handle;
+        return 233;
+    }
+    void perf_lock_cmd() {}
+    int perf_lock_rel(int handle) {
+        if (handle > 0)
+            return handle;
+        return 233;
+    }
+    void perf_lock_use_profile() {}
 }
