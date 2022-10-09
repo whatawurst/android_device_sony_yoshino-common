3c170023bbb872f25ee3401bfd48e91dd8408012
Shashank Verma <shashank.verma2002@gmail.com>
2022-10-09 23:34:04 +0530

    yoshino-common: OpenCS: XperiaParts: Call internal functions every time re-apply modem setting is toggled.
    The setting was only effective on second boot after change.
    Now, user will be forced to reboot when turning on the setting, calling the `reApplyModem` function as well.
    And disabling the setting, will not reboot, and call `revertReApplyModem`.
    
    The reboot while enabling the setting will also prevent users from toggling it too frequently
    that would rather be resulting in inconsistency and breaking their modem.
    
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- hardware/OpenCustomizationSelector/src/com/sonymobile/customizationselector/PreferenceReceiver.java
+++ hardware/OpenCustomizationSelector/src/com/sonymobile/customizationselector/PreferenceReceiver.java
@@ -3,6 +3,7 @@ package com.sonymobile.customizationselector;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
 import android.telephony.SubscriptionManager;
 
 public class PreferenceReceiver extends BroadcastReceiver {
@@ -18,21 +19,35 @@ public class PreferenceReceiver extends BroadcastReceiver {
             return;
         }
 
-        int change = intent.getIntExtra("cs_ims", DISABLED);
-        CSLog.d(TAG, "change received: " + change);
-        int subID = context.createDeviceProtectedStorageContext().getSharedPreferences(Configurator.PREF_PKG, Context.MODE_PRIVATE)
-                .getInt("event_subID", SubscriptionManager.INVALID_SUBSCRIPTION_ID);
-        if (subID == SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
-            CSLog.e(TAG, "Invalid sub ID, returning");
+        int pref = intent.getIntExtra("pref", -1);
+        if (pref == -1) {
+            CSLog.d(TAG, "Invalid pref, returning ...");
             return;
         }
 
-        ImsSwitcher switcher = new ImsSwitcher(context);
-        if (change == ENABLED) {
-            switcher.switchOnIMS(subID);
+        if (pref == 0) {
+            int change = intent.getIntExtra("cs_ims", DISABLED);
+            CSLog.d(TAG, "change received: " + change);
+            int subID = context.createDeviceProtectedStorageContext().getSharedPreferences(Configurator.PREF_PKG, Context.MODE_PRIVATE)
+                    .getInt("event_subID", SubscriptionManager.INVALID_SUBSCRIPTION_ID);
+            if (subID == SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
+                CSLog.e(TAG, "Invalid sub ID, returning");
+                return;
+            }
+
+            ImsSwitcher switcher = new ImsSwitcher(context);
+            if (change == ENABLED) {
+                switcher.switchOnIMS(subID);
+            }
+            if (change == DISABLED) {
+                switcher.switchOffIMS();
+            }
         }
-        if (change == DISABLED) {
-            switcher.switchOffIMS();
+        if (pref == 1) {
+            int apply = intent.getIntExtra("cs_re_apply_modem", DISABLED);
+            if (apply == ENABLED) ModemSwitcher.reApplyModem(context);
+            if (apply == DISABLED) ModemSwitcher.revertReApplyModem(context);
+            (context.getSystemService(PowerManager.class)).reboot(context.getString(R.string.reboot_reason));
         }
     }
 }
