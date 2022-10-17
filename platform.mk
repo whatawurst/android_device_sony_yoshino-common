3f506dacfbf25af283976eb1e2a1d293df8068a5
Alexander Martinz <amartinz@shiftphones.com>
2022-10-17 18:16:15 +0200

    Revert "yoshino-common: Disable APEX compression"
    This reverts commit 083c919fb167ac06fa0afa61f9185b6875b5b73e.
    
    Compressed APEX support is fixed kernel side.
    
    Test: build, flash and boot device with flag reverted.
    Test: boot GSI with compressed APEX enabled.
    
    Change-Id: I5988614cc055d565b17a5c8d830e35feb0e33e92
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- platform.mk
+++ platform.mk
@@ -29,8 +29,6 @@ QCOM_SOONG_NAMESPACE := $(PLATFORM_PATH)/hardware/qcom-caf
 ### APEX
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-# Disable APEX compression
-PRODUCT_COMPRESSED_APEX := false
 
 ### DALVIK
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
